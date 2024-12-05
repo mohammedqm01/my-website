82676ab28753cc47289fae4c396dfde960a7d8a7
Mohammed <102700844+mohammedqm01@users.noreply.github.com>
2024-12-05 21:37:30 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Ensure the "Own Franchise" button works
+document.getElementById("franchise-btn").addEventListener("click", function () {
+    window.location.href = "franchise-details.html"; // Redirect to franchise-details page
+});
